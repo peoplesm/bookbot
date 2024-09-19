5ec3e0b82b599407e6cea72a3b8eb4b0ff24c624
peoplesm <mep.summit@gmail.com>
2024-09-18 18:20:03 -0600

    Char counter

--- main.py
+++ main.py
@@ -2,19 +2,31 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = get_word_count(text)
+    letter_dict = get_char_count(text)
     print(text)
     print(f"{word_count} words found in the document")
+    print(letter_dict)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-    
+
+
 def get_word_count(text):
     words = text.split()
     return len(words)
 
 
+def get_char_count(text):
+    letters = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in letters:
+            letters[lowered] += 1
+        else:
+            letters[lowered] = 1
+    return letters
 
 
 main()
