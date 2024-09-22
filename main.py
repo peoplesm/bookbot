6a680987a9648e360ec8c67f931855dac2ed438e
peoplesm <mep.summit@gmail.com>
2024-09-22 09:28:25 -0600

    Added Book Report function

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     letter_dict = get_char_count(text)
+    book_report = get_book_report(letter_dict)
     print(text)
+    print(f"--- Begin report of {book_path} ---")
     print(f"{word_count} words found in the document")
-    print(letter_dict)
+    print()
+
+    for i in range(0, len(book_report), 1):
+        print(
+            f"The '{book_report[i].get('Character')}' character was found {book_report[i].get('Count')} times"
+        )
 
 
 def get_book_text(path):
@@ -26,6 +33,22 @@ def get_char_count(text):
             letters[lowered] += 1
         else:
             letters[lowered] = 1
+    return letters
+
+
+def get_book_report(letter_dict):
+
+    letters = [
+        {"Character": key, "Count": value}
+        for key, value in letter_dict.items()
+        if key.isalpha()
+    ]
+
+    def sort_on(dict):
+        return dict["Count"]
+
+    letters.sort(reverse=True, key=sort_on)
+
     return letters
 
 
